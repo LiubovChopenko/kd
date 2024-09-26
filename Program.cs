36754e002b4525e7ceb9be120401bef12eb6d8f1
User <User@DESKTOP-0FB8NOE>
2024-09-26 04:49:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int k = 1; k <= 99; k++)
+            {
+                Console.WriteLine($"Мені {k} {DetermineYearWord(k)}");
+            }
+        }
+
+        static string DetermineYearWord(int k)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            if (k % 10 == 1 && k != 1)
+            {
+                return "рік";
+            }
+            else if ((k % 10 >= 2 && k % 10 <= 4) && !(k >= 12 && k <= 14))
+            {
+                return "роки";
+            }
+            else
+                return "років";
+            Console.ReadKey();
+            {
+
+
+
+            }
+        }
+    }
+}
+
